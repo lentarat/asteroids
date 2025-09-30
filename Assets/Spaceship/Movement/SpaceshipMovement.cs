c5cc88ae272fe1f3eed11868d248572b171f8746
lentarat <126574303+lentarat@users.noreply.github.com>
2025-09-30 23:55:50 +0300

    Spaceship movement

--- Assets/Spaceship/Movement/SpaceshipMovement.cs
+++ Assets/Spaceship/Movement/SpaceshipMovement.cs
@@ -2,8 +2,35 @@ using UnityEngine;
 
 public class SpaceshipMovement
 {
-    public SpaceshipMovement()
+    private SpaceshipMovementSO _spaceshipMovementSO;
+    private ISpaceshipMover _spaceshipMover;
+
+    public SpaceshipMovement(SpaceshipMovementSO spaceshipMovementSO, ISpaceshipMover spaceshipMover)
     { 
-    
+        _spaceshipMovementSO = spaceshipMovementSO;
+        _spaceshipMover = spaceshipMover;
+
+        SubscribeToMovementInput();    
+    }
+
+    private void SubscribeToMovementInput()
+    {
+        _spaceshipMover.OnMove += HandleSpaceshipMoved;
+        _spaceshipMover.OnRotate += HandleSpaceshipRotated;
+    }
+
+    private void HandleSpaceshipMoved(float yLocal)
+    {
+        
+    }
+
+    private void HandleSpaceshipRotated(float zRotation)
+    { 
+        
+    }
+
+    private void Move()
+    { 
+        
     }
 }
